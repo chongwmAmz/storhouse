c67f0b42585990e63d1afb0f0c3ee67a7a126c4e
chongwmAmz <118857753+chongwmAmz@users.noreply.github.com>
2022-12-08 15:45:24 -0500

    Update 4.createEfsCsi.sh

--- 4.createEfsCsi.sh
+++ 4.createEfsCsi.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -x
-#curl https://raw.githubusercontent.com/chongwmAmz/storhouse/main/alfresco-pv.yaml | kubectl apply -f -
 wget https://raw.githubusercontent.com/chongwmAmz/storhouse/main/alfresco-pv.yaml
-curl https://rhttps://raw.githubusercontent.com/chongwmAmz/storhouse/main/alfresco-pv.yamlaw.githubusercontent.com/kubernetes-sigs/aws-efs-csi-driver/master/examples/kubernetes/multiple_pods/specs/storageclass.yaml | kubectl apply -f -
-#wget -P aws-efs-csi-driver/examples/kubernetes/multiple_pods/specs/ https://eksworkshop.com/beginner/190_efs/efs.files/efs-reader.yaml
-#sed -i "/namespace:/c\  namespace: alfresco" aws-efs-csi-driver/examples/kubernetes/multiple_pods/specs/efs-reader.yaml
+sed -i "/volumeHandle:/c\    volumeHandle: $1" alfresco-pv.yaml
+kubectl apply -f alfresco-pv.yaml
+curl https://raw.githubusercontent.com/kubernetes-sigs/aws-efs-csi-driver/master/examples/kubernetes/multiple_pods/specs/storageclass.yaml | kubectl apply -f -
 kubectl create namespace alfresco
-#kubectl apply -f aws-efs-csi-driver/examples/kubernetes/multiple_pods/specs/efs-reader.yaml
 curl https://raw.githubusercontent.com/chongwmAmz/storhouse/main/alfresco-claim.yaml | kubectl apply -f -
 set +x
