9839557f85be40cf7a19d93d408f3cccf196b870
chongwmAmz <118857753+chongwmAmz@users.noreply.github.com>
2022-12-08 14:16:42 -0500

    Update makeFileShareDirectories.sh

--- makeFileShareDirectories.sh
+++ makeFileShareDirectories.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo mkdir -p /mnt/sharedfilestore/alfresco-content-services/filestore-data \
  /mnt/sharedfilestore/alfresco-content-services/repository-data
-sudo chown -R 33000:1000 /mnt/sharedfilestore/alfresco-content-services/filestore-data \
- /mnt/sharedfilestore/alfresco-content-services/repository-data
+sudo chown -R 33000:1000 /mnt/sharedfilestore/alfresco-content-services
 sudo chmod -R g+w /mnt/sharedfilestore/alfresco-content-services/
