7deaf106ab227863c0c4db68f13bfcf2e2724422
chongwmAmz <118857753+chongwmAmz@users.noreply.github.com>
2022-12-09 17:55:41 -0500

    Update 1.createCluster.sh

--- 1.createCluster.sh
+++ 1.createCluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-eksctl create cluster -f eksctl-createcluster-fgnonode.yaml 
+eksctl create cluster -f eksctl-createcluster-nonode-apse1fg.yaml
 eksctl utils associate-iam-oidc-provider \
     --region $1 \
     --cluster $2 \
