621421451932d6060320a9c18059786fdbb080b4
chongwmAmz <118857753+chongwmAmz@users.noreply.github.com>
2022-12-12 16:00:33 -0500

    Update 4.createEfsCsi.sh

--- 4.createEfsCsi.sh
+++ 4.createEfsCsi.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -x
 wget https://raw.githubusercontent.com/chongwmAmz/storhouse/main/alfresco-pv.yaml
 sed -i "/volumeHandle:/c\    volumeHandle: $1" alfresco-pv.yaml
 kubectl apply -f alfresco-pv.yaml
 curl https://raw.githubusercontent.com/kubernetes-sigs/aws-efs-csi-driver/master/examples/kubernetes/multiple_pods/specs/storageclass.yaml | kubectl apply -f -
 kubectl create namespace alfresco
-curl https://raw.githubusercontent.com/chongwmAmz/storhouse/main/alfresco-claim.yaml | kubectl apply -f -
-set +x
+curl https://raw.githubusercontent.com/chongwmAmz/storhouse/main/alfresco-claim.yaml | kubectl apply -f -curl 
+curl https://raw.githubusercontent.com/chongwmAmz/storhouse/main/alfresco-dir-creator.yaml | kubectl apply -f -
