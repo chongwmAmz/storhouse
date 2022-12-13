61e8c9309a23020f06f1a59848725cb509fc4cad
chongwmAmz <118857753+chongwmAmz@users.noreply.github.com>
2022-12-12 19:02:55 -0500

    Create deleteCLBIngresses.sh

---
+++ deleteCLBIngresses.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+kubectl -n alfresco delete ingress acs-alfresco-sync-service-ingress
+kubectl -n alfresco delete ingress acs-alfresco-cs-repository
+kubectl -n alfresco delete ingress acs-alfresco-cs-share
+kubectl -n alfresco delete ingress acs-alfresco-dw
+kubectl -n alfresco delete ingress acs-alfresco-sync-service-ingress
