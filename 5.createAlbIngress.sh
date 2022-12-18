57639806248861145f1ed3de260e5fc9812c00ec
chongwmAmz <118857753+chongwmAmz@users.noreply.github.com>
2022-12-18 01:34:29 -0500

    Update 5.createAlbIngress.sh

--- 5.createAlbIngress.sh
+++ 5.createAlbIngress.sh
@@ -2,5 +2,5 @@
 kubectl -n alfresco annotate svc acs-alfresco-cs-share alb.ingress.kubernetes.io/healthcheck-path=/share alb.ingress.kubernetes.io/success-codes='200,302'
 certARN=$1
 wget https://raw.githubusercontent.com/chongwmAmz/storhouse/main/acs-alfresco-alb-ingress-https.yaml
-sed -i "s/<ARN_Of_Certificate>/$certARN/g" acs-alfresco-alb-ingress-https.yaml
+sed -i "s#<ARN_Of_Certificate>#$certARN#g" acs-alfresco-alb-ingress-https.yaml
 kubectl apply -f acs-alfresco-alb-ingress-https.yaml
