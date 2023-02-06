ec65429a036ad7cb51d65531eb1dba8da1e12fc7
chongwmAmz <118857753+chongwmAmz@users.noreply.github.com>
2023-02-06 18:26:59 -0500

    Update 7.retrieveCert.sh

--- 7.retrieveCert.sh
+++ 7.retrieveCert.sh
@@ -2,3 +2,7 @@ DOMAIN_NAME=$1
 aws acm list-certificates --query \
 "CertificateSummaryList[?DomainName == '$DOMAIN_NAME']" \
 | jq -r '.[].CertificateArn'
+
+aws acm list-certificates --query \
+"CertificateSummaryList[?SubjectAlternativeNameSummaries[?contains(@, '$DOMAIN_NAME')]][].CertificateArn" \
+| jq -r '.[].CertificateArn'
